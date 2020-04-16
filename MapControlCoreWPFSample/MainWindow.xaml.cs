cdb634f5645633100e31464baa5ca0424becf09b
luisquintanilla <Luis.Quintanilla@microsoft.com>
2020-04-16 16:58:58 -0400

    Initial commit

---
+++ MapControlCoreWPFSample/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT;
+using Microsoft.Toolkit.Wpf.UI.Controls;
+using System.Windows;
+
+namespace MapControlCoreWPFSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void MapControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Specify a known location.
+            BasicGeoposition cityPosition = new BasicGeoposition() { Latitude = 47.604, Longitude = -122.329 };
+            var cityCenter = new Geopoint(cityPosition);
+
+            // Set the map location.
+            await (sender as MapControl).TrySetViewAsync(cityCenter);
+        }
+    }
+}
